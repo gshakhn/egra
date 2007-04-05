37bc9251013738dd68d5d8808b3859692d14856a
gshakhn <gshakhn@7d391932-2d2d-0410-8dbf-c741425e219c>
2007-04-05 03:13:39 +0000

    Updated ItemType so each type has a corresponding number.

--- trunk/Core/ItemType.java
+++ trunk/Core/ItemType.java
@@ -5,5 +5,39 @@
  */
 
 public enum ItemType {
-	MISC, AMULET, BOOTS, BRACERS, CHEST, GLOVES, GOGGLES, HAT, MELEE_WEAPON, PANTS, RING, SHIELD, SHOULDER, SOCKS, BELT, EARRING;
+	MISC (0),
+	AMULET (1),
+	BOOTS (2),
+	BRACERS (3),
+	CHEST (4),
+	GLOVES (5),
+	GOGGLES (6),
+	HAT (7),
+	MELEE_WEAPON (8),
+	PANTS (9),
+	RING (10),
+	SHIELD (11),
+	SHOULDER (12),
+	SOCKS (13),
+	BELT (14),
+	EARRING (15);
+	
+	private final int num;
+	
+	private static final int numTypes = 16;
+	
+	ItemType(int num)
+	{
+		this.num = num;
+	}
+	
+	public int num()
+	{
+		return num;
+	}
+	
+	public static int numTypes()
+	{
+		return numTypes;
+	}
 }
