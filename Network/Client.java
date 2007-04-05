28369d0d471e6ef3fa82e5b0f1ca6c93082d0626
gshakhn <gshakhn@7d391932-2d2d-0410-8dbf-c741425e219c>
2007-04-05 02:55:01 +0000

    Updated /Network with correct files.

---
+++ Network/Client.java
@@ -0,0 +1,107 @@
+import javax.swing.JApplet;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * The client.
+ * 
+ * @author George Shakhnazaryan
+ */
+
+public class Client extends JApplet implements Runnable {
+
+	private Socket socket;
+	private final int PORT = 31415;
+	private final String HOST = "www.react-z.com";
+	
+	private final String ClientVersion = "1.0";
+	
+	private WorldCell currentCell;
+	
+	private Player player;
+	
+	BufferedReader in;
+	BufferedWriter out;
+	
+	private final int SLEEPTIME = 100;
+	
+	public void init()
+	{
+		try
+		{
+			socket = new Socket (HOST,PORT);
+			
+			try
+			{
+				in = new BufferedReader(new InputStreamReader(
+						socket.getInputStream()));
+				out = new BufferedWriter(new OutputStreamWriter(
+						socket.getOutputStream()));
+				
+				out.write(OpCodes.SENDVERSIONNUMBER);
+				
+				boolean versionInfoRecieved = false;
+				
+				while (!versionInfoRecieved)
+				{
+					try
+					{
+						Thread.sleep(SLEEPTIME);
+					}
+					catch (InterruptedException ex) {}
+					
+					String serverVersion = "";
+					if (in.ready())
+						serverVersion = in.readLine();
+					
+					if (!serverVersion.equals("") &&
+							(serverVersion.substring(0,OpCodes.OPCODELENGTH)).equals(OpCodes.SENDVERSIONNUMBER))
+					{
+						int versionLength = Integer.parseInt(
+								serverVersion.substring(OpCodes.OPCODELENGTH,OpCodes.OPCODELENGTH+1));
+						serverVersion = serverVersion.substring(
+								OpCodes.OPCODELENGTH+1,OpCodes.OPCODELENGTH+1+versionLength);
+						
+						if (!serverVersion.equals(ClientVersion))
+						{
+							outOfDateClient();
+							in.close();
+							out.close();
+							socket.close();
+						}
+						else
+						{
+							versionInfoRecieved = true;
+						}
+					}
+				}
+			}
+			catch (IOException ex)
+			{
+				ex.toString();
+			}
+		}
+		catch (IOException ex)
+		{
+			ex.toString();
+		}
+	}
+	
+	/**
+	 * Message is sent to client to ask them to upgrade.
+	 * Connections then close.
+	 */
+	private void outOfDateClient()
+	{
+		// Calvin and Cult are out of date.
+	}
+	
+	public void run()
+	{
+
+	}
+}
---
+++ Network/PlayerHandler.java
@@ -0,0 +1,28 @@
+import java.net.Socket;
+
+/**
+ * Contains reference to the Player and to the Socket of that player.
+ *
+ * @author George Shakhnazaryan
+ *
+ */
+public class PlayerHandler {
+	
+	public Player player;
+	public Socket socket;
+	
+    public String toSend;
+	
+	public PlayerHandler (Player player, Socket socket)
+	{
+		this.player = player;
+		this.socket = socket;
+		
+		player.ph = this;
+        toSend = "";
+	}
+	
+	public void close()
+	{
+	}
+}
